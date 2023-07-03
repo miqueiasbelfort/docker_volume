5240fcbf7d464b2bd92afef4e72a745e00f399e5
Miqueias Kawa <kawasousa1554@gmail.com>
2023-07-03 18:27:55 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Messagens</title>
+</head>
+<body>
+    <h1>Escreva sua messagem</h1>
+    <form action="process.php" method="POST">
+        <input type="text" name="message" id="message">
+        <input type="submit" value="Enviar Messagem">
+    </form>
+</body>
+</html>
